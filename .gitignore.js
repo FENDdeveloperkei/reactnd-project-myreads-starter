b69eda0c5fbe061225b2c120792e48ad03714f91
Lakeshia Williams <38958988+FENDdeveloperkei@users.noreply.github.com>
2018-09-14 13:21:06 -0500

    Rename .gitignore to .gitignore.js

---
+++ .gitignore.js
@@ -0,0 +1,22 @@
+# Use https://help.github.com/ignore-files/ for more help with ignoring files.
+
+# dependencies
+/node_modules
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
