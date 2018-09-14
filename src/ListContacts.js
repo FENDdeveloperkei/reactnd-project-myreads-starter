6e1de096dadf671f532c30f9dccc6969e8719923
Lakeshia Williams <38958988+FENDdeveloperkei@users.noreply.github.com>
2018-09-14 13:26:19 -0500

    Update ListContacts.js

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,84 +1,117 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import React from 'react'
 import PropTypes from 'prop-types'
-import escapeRegExp from 'escape-string-regexp'
-import sortBy from 'sort-by'
 
+const readFileAsDataURL = (file) =>
+  new Promise(resolve => {
+    const reader = new FileReader()
 
-class ListContacts extends Component {
+    reader.onload = (event) => {
+      resolve(event.target.result)
+    }
+
+    reader.readAsDataURL(file)
+  })
+
+const resizeImage = (imageURL, canvas, maxHeight) =>
+  new Promise(resolve => {
+    const image = new Image()
+
+    image.onload = () => {
+      const context = canvas.getContext('2d')
+
+      if (image.height > maxHeight) {
+        image.width *= maxHeight / image.height
+        image.height = maxHeight
+      }
+
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      canvas.width = image.width
+      canvas.height = image.height
+
+      context.drawImage(image, 0, 0, image.width, image.height)
+
+      resolve(canvas.toDataURL('image/jpeg'))
+    }
+
+    image.src = imageURL
+  })
+
+/**
+ * A custom <input> that dynamically reads and resizes image files before
+ * submitting them to the server as data URLs. Also, shows a preview of the image.
+ */
+class ImageInput extends React.Component {
   static propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
+    className: PropTypes.string,
+    name: PropTypes.string,
+    maxHeight: PropTypes.number
   }
 
   state = {
-    query: ''
+    value: ''
   }
 
-  updateQuery = (query) => {
-    this.setState({ query: query.trim() })
+  handleFileChange = (event) => {
+    const file = event.target.files[0]
+
+    if (file && file.type.match(/^image\//)) {
+      readFileAsDataURL(file).then(originalURL => {
+        resizeImage(originalURL, this.canvas, this.props.maxHeight).then(url => {
+          this.setState({ value: url })
+        })
+      })
+    } else {
+      this.setState({ value: '' })
+    }
   }
 
-  clearQuery = () => {
-    this.setState({ query: '' })
+  handleFormReset = () => {
+    this.setState({ value: '' })
+  }
+
+  componentDidMount() {
+    this.canvas = document.createElement('canvas')
+    this.fileInput.form.addEventListener('reset', this.handleFormReset)
+  }
+
+  componentWillUnmount() {
+    this.fileInput.form.removeEventListener('reset', this.handleFormReset)
   }
 
   render() {
-    const { contacts, onDeleteContact } = this.props
-    const { query } = this.state
+    const { className, name } = this.props
+    const { value } = this.state
 
-    let showingContacts
-    if (query) {
-      const match = new RegExp(escapeRegExp(query), 'i')
-      showingContacts = contacts.filter((contact) => match.test(contact.name))
-    } else {
-      showingContacts = contacts
+    const style = {
+      position: 'relative'
     }
 
-    showingContacts.sort(sortBy('name'))
+    if (value) {
+      style.backgroundImage = `url("${value}")`
+      style.backgroundRepeat = 'no-repeat'
+      style.backgroundPosition = 'center'
+      style.backgroundSize = 'cover'
+    }
 
     return (
-      <div className='list-contacts'>
-        <div className='list-contacts-top'>
-          <input
-            className='search-contacts'
-            type='text'
-            placeholder='Search contacts'
-            value={query}
-            onChange={(event) => this.updateQuery(event.target.value)}
-          />
-          <Link
-            to='/create'
-            className='add-contact'
-          >Add Contact</Link>
-        </div>
-
-        {showingContacts.length !== contacts.length && (
-          <div className='showing-contacts'>
-            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
-            <button onClick={this.clearQuery}>Show all</button>
-          </div>
-        )}
-
-        <ol className='contact-list'>
-          {showingContacts.map((contact) => (
-            <li key={contact.id} className='contact-list-item'>
-              <div className='contact-avatar' style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}/>
-              <div className='contact-details'>
-                <p>{contact.name}</p>
-                <p>{contact.email}</p>
-              </div>
-              <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
-                Remove
-              </button>
-            </li>
-          ))}
-        </ol>
+      <div className={className} style={style}>
+        <input type="hidden" name={name} value={value} />
+        <input
+          ref={node => this.fileInput = node}
+          type="file"
+          onChange={this.handleFileChange}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            opacity: 0
+          }}
+        />
       </div>
     )
   }
 }
 
-export default ListContacts
+export default ImageInput
